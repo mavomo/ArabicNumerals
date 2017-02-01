733c709e9b13b33d17b8acb26ec2655253692362
mavomo <michelle.avomo@gmail.com>
2017-02-01 13:29:36 +0100

    Refactor I cases

--- src/main/java/io/training/katas/ArabicNumerals.java
+++ src/main/java/io/training/katas/ArabicNumerals.java
@@ -1,16 +1,27 @@
 package io.training.katas;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Michelle on 01/02/2017.
  */
 public class ArabicNumerals {
     public static int convertFromRoman(String romanNumber) {
+        Map<String, Integer> arabicNumbersFromRomanChart = new HashMap<>();
+        arabicNumbersFromRomanChart.put("I", 1);
+        char[] romanNumerals = romanNumber.toCharArray();
+        int value = arabicNumbersFromRomanChart.get("I");
+        for (int i = 1 ; i < romanNumerals.length; i++){
+            value++;
+        }
+/*
         if (romanNumber == "II"){
             return 2;
         }
         if (romanNumber == "III"){
             return 3;
-        }
-        return 1;
+        }*/
+        return value;
     }
 }
--- src/test/java/io/training/katas/ArabicNumeralTest.java
+++ src/test/java/io/training/katas/ArabicNumeralTest.java
@@ -1,6 +1,5 @@
 package io.training.katas;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
